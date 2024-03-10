686ea49e26d012fef377afe6eb3186d90ba9af20
Young Lee <lee.youngx@gmail.com>
2024-03-09 22:56:35 -0700

    Apply black formatting

--- vrp.py
+++ vrp.py
@@ -11,33 +11,35 @@ class Point:
     def get_drive_time(self, _point: "Point"):
         """Returns the drive time in minutes one Point to another Point."""
 
-        return math.sqrt((_point.x - self.x)**2 + (_point.y - self.y)**2)
-    
+        return math.sqrt((_point.x - self.x) ** 2 + (_point.y - self.y) ** 2)
+
 
 class Load:
     def __init__(self, load_number: int, pickup: Point, dropoff: Point):
         self.load_number = load_number
         self.pickup = pickup
         self.dropoff = dropoff
-        self.duration = self.pickup.get_drive_time(self.dropoff) # Drive time from pickup to dropoff
+        self.duration = self.pickup.get_drive_time(
+            self.dropoff
+        )  # Drive time from pickup to dropoff
 
     def get_drive_time(self, _load: "Load"):
         """Returns the drive time in minutes one Load to another Load.
-        
+
         Distinction is made between Point and Load due to Load having a 'pickup'
         and 'dropoff' attributes - these must be considered when calculating
         drive time between two Loads.
-    
+
         """
 
         return self.dropoff.get_drive_time(_load.pickup)
-    
+
     def get_nearest_load(self, _loads: list["Load"]):
         """Finds and returns the nearest Load in terms of drive time.
-        
+
         Returns the nearest Load instance and the drive time in minutes to drive
         from the current Load to the nearest Load instance.
-        
+
         """
 
         min_drive_time = float("inf")
@@ -52,9 +54,9 @@ class Load:
 
         return nearest_load, min_drive_time
 
-    
-MAX_SHIFT_TIME = 12.0 * 60.0 # Shift maximum is 12 hours
-DEPOT = Load(-1, Point(0.0, 0.0), Point(0.0, 0.0)) # Location drivers start and end
+
+MAX_SHIFT_TIME = 12.0 * 60.0  # Shift maximum is 12 hours
+DEPOT = Load(-1, Point(0.0, 0.0), Point(0.0, 0.0))  # Location drivers start and end
 
 
 def solve_vrp(loads):
@@ -63,7 +65,7 @@ def solve_vrp(loads):
 
     driver = 0
     while incomplete_loads:
-        current = DEPOT # All drivers start at (0,0)
+        current = DEPOT  # All drivers start at (0,0)
         schedules.append([])
         total_drive_time = 0.0
 
@@ -72,7 +74,12 @@ def solve_vrp(loads):
             if nearest_load is None:
                 # No valid nearest Load was found
                 break
-            elif (total_drive_time + drive_time + nearest_load.duration + nearest_load.get_drive_time(DEPOT)) > MAX_SHIFT_TIME:
+            elif (
+                total_drive_time
+                + drive_time
+                + nearest_load.duration
+                + nearest_load.get_drive_time(DEPOT)
+            ) > MAX_SHIFT_TIME:
                 # Invalid schedule if driver is not able to stay under maximum 12 hour shift
                 # This includes returning back to (0,0)
                 break
@@ -85,23 +92,25 @@ def solve_vrp(loads):
         driver += 1
 
     return schedules
-            
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("path_to_problem", help="Path to text file containing VRP problem")
+    parser.add_argument(
+        "path_to_problem", help="Path to text file containing VRP problem"
+    )
     args = parser.parse_args()
 
     # Load problem from text file
     with open(Path(args.path_to_problem), "r") as f:
         loads = []
         for line in f:
+            # Skip the header row
+            if "loadNumber" in line:
+                continue
+
             load_number, pickup, dropoff = line.split(" ")
 
-            # Skip the header row
-            if load_number == "loadNumber":
-                continue
-            
             # Load coordinates as tuples
             # Assumption is correctly defined coordinates - 'eval()' is generally not recommended
             pickup = eval(pickup)
@@ -112,5 +121,3 @@ if __name__ == '__main__':
     solution = solve_vrp(loads)
     for schedule in solution:
         print(schedule)
-
-    
